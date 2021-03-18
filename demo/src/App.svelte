e1770b7aaf140533a274845ce36e7661f57510a0
V-ed <guillaumemarcoux@gmail.com>
2021-03-18 10:57:13 -0400

    Convert demo to HelloWorld Svelte app

---
+++ demo/src/App.svelte
@@ -0,0 +1,5 @@
+<script>
+	import HelloWorld from "$/HelloWorld.svelte";
+</script>
+
+<HelloWorld />
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,10 +1,17 @@
-import tsconfigPaths from 'vite-tsconfig-paths'
-import reactRefresh from '@vitejs/plugin-react-refresh'
-import type { UserConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
+import svelte from '@svitejs/vite-plugin-svelte';
+import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
+  resolve: {
+    alias: {
+      svelte: resolve(__dirname, 'node_modules/svelte'),
+      $: resolve(__dirname, './src'),
+    }
+  },
   plugins: [
-    reactRefresh(), //
+    svelte(),
     tsconfigPaths(),
   ],
 }
